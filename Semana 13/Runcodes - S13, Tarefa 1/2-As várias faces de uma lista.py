2e81172bac2b27970e5117e29d7f8a21ae593dc9
Jill_ZAP <franciscojusepi@gmail.com>
2025-11-26 23:00:40 -0300

    Add files via upload
    Questão de fácil entendimento, mas de péssima execução de minha parte.

--- Semana 13/Runcodes - S13, Tarefa 1/2-As várias faces de uma lista.py
+++ Semana 13/Runcodes - S13, Tarefa 1/2-As várias faces de uma lista.py
@@ -1,35 +1,55 @@
 #Processamento de dados
-def lista_A(L):
-    L.insert(0, 0)
-    return L
+def lista_A(tt):
+    #Lista VAZIA
+    A = []
+    #Adiciona itens na lista de acordo com o total desejado
+    for i in range(tt):
+        A.append(i*0)
+    #Retorna a lista contendo apenas um conjunto de números ZERO
+    return A
 
-def lista_B(L):
-    L.insert(0, 1)
-    return L
+def lista_B(tt):
+    #Lista VAZIA
+    B = []
+    #Adiciona itens na lista de acordo com o total desejado
+    for i in range(tt):
+        B.append(i+1)
+    #Retorna a lista contendo os números de 1 até total almejado
+    return B
 
-def lista_C(L):
-    return L
+def lista_C(tt):
+    #Lista VAZIA
+    C = []
+    #Adiciona itens na lista de acordo com o total desejado
+    for i in range(tt):
+        numero = int(input("Digite o {i+1}º número: ")
+        C.append(numero)
+    #Retorna a lista contendo as variáveis digitadas
+    return C
 
-def lista_D(L):
-    return L.reverse()
+def lista_D(tt):
+    #Lista VAZIA
+    D = []
+    #Adiciona itens na lista de acordo com o total desejado
+    for i in range(tt):
+        numero = int(input("Digite o {i+1}º número: "))
+        D.append(numero)
+    #Os últimos serão os primeiros
+    D.reverse()
+    #Retorna a lista contendo as variáveis em ordem invertida
+    return D
 
 #Programa principal
 def main():
-    #Dita o número de variáveis que a lista receberá
-    n_itens = int(input("Quantos itens? "))
+    #Determina o número de itens da lista
+    total = int(input("Quantos itens cada lista deve ter? "))
 
-    #Lista VAZIA
-    lista = []
-
-    for i in range(n_itens):
-        numero = int(input("Digite um número: "))
-        lista.append(numero)
-    
     #Imprime as listas
-    print(lista_A(lista))
-    print(lista_B(lista))
-    print(lista_C(lista))
-    print(lista_D(lista))
+    print("Está lista revela meu saldo bancário:", lista_A(total))
+    print(lista_B(total))
+    print(lista_C(total))
+    print(lista_D(total))
 
 if __name__ == '__main__':
     main()
+    
