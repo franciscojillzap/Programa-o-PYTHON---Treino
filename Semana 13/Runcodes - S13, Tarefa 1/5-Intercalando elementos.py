6be29fc9385079ba1d8699a5f747ab13778b8ba2
Jill_ZAP <franciscojusepi@gmail.com>
2025-11-28 07:56:22 -0300

    Questão abençoada
    zip() - capaz de intercalar os itens de duas listas, salvou minha pele

--- Semana 13/Runcodes - S13, Tarefa 1/5-Intercalando elementos.py
+++ Semana 13/Runcodes - S13, Tarefa 1/5-Intercalando elementos.py
@@ -3,26 +3,34 @@ def lista_A():
     A = []
     
     for i in range(2):
-        letras = input()
+        letras = int(input())
         A.append(letras)
+    
+    return A
 
 def lista_B():
     B = []
     
     for i in range(2):
-        letras = input()
+        letras = int(input())
         B.append(letras)
+    
+    return B
 
 def lista_C():
+    A = lista_A()
+    B = lista_B()
     C = []
     
-    for item_A, item_B in zip(lista_A, lista_B):
+    for item_A, item_B in zip(A, B):
         C.append(item_A)
         C.append(item_B)
+    
+    return C
         
 #Programa principal
 def main():
-    print(lista_C)
+    print(lista_C())
 
 if __name__ == '__main__':
     main()
