8b5c760dfe3674e0e71754619192f4e26ec3927f
Jill_ZAP <franciscojusepi@gmail.com>
2025-12-06 12:12:19 -0300

    Add files via upload

--- Semana 15/Runcodes - S15, Tarefa 1/5-VAMOS FAZER UMA FESTA, e nela transcender os limites populacionais.py
+++ Semana 15/Runcodes - S15, Tarefa 1/5-VAMOS FAZER UMA FESTA, e nela transcender os limites populacionais.py
@@ -18,20 +18,20 @@ def cidades_aniversarigrandes(M, POP):
     MES = nome_dos_meses(M).upper()
     lista_meganiver = []
     
-    print(f'CIDADES COM MAIS DE {POP} E ANIVERSÁRIO EM {MES}:')
+    print(f'\nCIDADES COM MAIS DE {POP} HABITANTES E ANIVERSÁRIO EM {MES}:\n')
 
     for uf, ibge, nome, dia, mes, pop in carrega_cidades():
-        if mes == MES and pop > POP:
-            lista_meganiver.append((f'{nome}({uf})', pop, dia, nome_dos_meses(M).lower()))
+        if mes == M and pop > POP:
+            lista_meganiver.append((f'{nome}({uf})', pop, dia, mes))
 
     return lista_meganiver
     
 def main():
-    mesversario = int(input())
-    populacao = int(input())
+    mesversario = int(input('Digite um valor referente a um MÊS: '))
+    populacao = int(input('Valor populacional de comparação: '))
     
     for cidade, teto_pop, dia, mes in cidades_aniversarigrandes(mesversario, populacao):
-        print(f'{cidade} tem {teto_pop} habitantes e faz aniversário em {dia} de {mes}.')
+        print(f'{cidade} tem {teto_pop} habitantes e faz aniversário em {dia} de {nome_dos_meses(mes)}.')
 
 if __name__ == '__main__':
     main()
